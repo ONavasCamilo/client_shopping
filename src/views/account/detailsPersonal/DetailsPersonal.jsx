de340614ad6c8d2b29047d18034403ef58dd00e2
Camilo Navas <navasochoa@hotmail.com>
2024-09-19 14:53:46 -0500

    axios update user and fix forms view detailsPersonal

--- src/views/account/detailsPersonal/DetailsPersonal.jsx
+++ src/views/account/detailsPersonal/DetailsPersonal.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../../../components/buttons/Button";
 import InputTextLogin from "../../../components/inputText/InputText";
 import SectionFlexDirection from "../../../components/sections/SectionFlexDirection";
 import SubtitleComponent from "../../../components/subtitle/SubtitleComponent";
 import TitleComponent from "../../../components/title/TitleComponent";
-import { useAuthContext, useSetAuthContext } from "../../../providers/UserProvider";
+import {
+  useAuthContext,
+  useSetAuthContext,
+} from "../../../providers/UserProvider";
 import axios from "axios";
-import { VITE_API_USERS_ADDDETAILSUSER, VITE_API_USERS_UPDATE_DETAILSUSER } from "../../../config/env.config";
+import {
+  VITE_API_USERS_ADDDETAILSUSER,
+  VITE_API_USERS_UPDATE,
+  VITE_API_USERS_UPDATE_DETAILSUSER,
+} from "../../../config/env.config";
 
 const DetailsPersonal = () => {
   const { user, token, detailsUser } = useAuthContext();
   const { handleOnAuthContext } = useSetAuthContext();
 
-  const initialState = {
+  const initialStateUser = {
+    name: user.name || "",
+    lastname: user.lastname || "",
+  };
+
+  const initialStateDetails = {
     detailsName: detailsUser?.detailsName || "",
     detailsLastname: detailsUser?.detailsLastname || "",
     address: detailsUser?.address || "",
@@ -20,32 +32,97 @@ const DetailsPersonal = () => {
     location: detailsUser?.location || "",
     country: detailsUser?.country || "",
     phone: detailsUser?.phone || "",
-  }
+  };
 
-  const [detailsData, setDetailsData] = useState(initialState);
+  const [userData, setUserData] = useState(initialStateUser);
+  const [detailsData, setDetailsData] = useState(initialStateDetails);
 
   const API_CALL = detailsUser?.detailsName
     ? VITE_API_USERS_UPDATE_DETAILSUSER + user.id
     : VITE_API_USERS_ADDDETAILSUSER + user.id;
   const method = detailsUser?.detailsName ? "PUT" : "POST";
 
+  useEffect(() => {
+    if (user) {
+      setUserData({
+        name: user.name || "",
+        lastname: user.lastname || "",
+      });
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (detailsUser) {
+      setDetailsData({
+        detailsName: detailsUser?.detailsName || "",
+        detailsLastname: detailsUser?.detailsLastname || "",
+        address: detailsUser?.address || "",
+        postalCode: detailsUser?.postalCode || "",
+        location: detailsUser?.location || "",
+        country: detailsUser?.country || "",
+        phone: detailsUser?.phone || "",
+      });
+    }
+  }, [detailsUser]);
+
+  if (!user || !token) {
+    return <p>Cargando...</p>;
+  }
+
   const handleOnSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await axios({
-        method,
-        url: API_CALL,
-        data: detailsData,
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      const updatedDetailsUser = response.data.detailUser;
-      window.localStorage.setItem("detailsUser", JSON.stringify(updatedDetailsUser));
-      handleOnAuthContext(true, user, token, updatedDetailsUser);
-      setDetailsData(initialState)
-    } catch (error) {
-      console.error("Error:", error);
+
+    const hasUserDataChanged =
+      userData.name !== initialStateUser.name ||
+      userData.lastname !== initialStateUser.lastname;
+
+    const hasDetailsDataChanged = Object.keys(detailsData).some(
+      (key) => detailsData[key] !== initialStateDetails[key]
+    );
+
+    let apiCallExecuted = false;
+
+    if (hasDetailsDataChanged) {
+      try {
+        const response = await axios({
+          method,
+          url: API_CALL,
+          data: detailsData,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const updatedDetailsUser = response.data.detailUser;
+        window.localStorage.setItem(
+          "detailsUser",
+          JSON.stringify(updatedDetailsUser)
+        );
+        handleOnAuthContext(true, user, token, updatedDetailsUser);
+        setDetailsData(initialStateDetails);
+        apiCallExecuted = true;
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    if (hasUserDataChanged) {
+      try {
+        const response = await axios.put(
+          VITE_API_USERS_UPDATE + user.id,
+          userData,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const updatedUser = response.data;
+        window.localStorage.setItem("user", JSON.stringify(updatedUser));
+        setUserData(initialStateUser)
+        apiCallExecuted = true;
+      } catch (error) {
+        console.log("Error:", error);
+      }
     }
   };
 
@@ -58,20 +135,21 @@ const DetailsPersonal = () => {
             placeholder="Nombre"
             name="name"
             type="text"
-            value={user.name}
+            userData={userData}
+            setUserData={setUserData}
           />
           <InputTextLogin
             placeholder="Apellidos"
             name="lastname"
             type="text"
-            value={user.lastname}
+            userData={userData}
+            setUserData={setUserData}
           />
           <SubtitleComponent text="Dirección de entrega por defecto" />
           <InputTextLogin
             placeholder="Nombre"
             name="detailsName"
             type="text"
-            value={detailsData.detailsName}
             userData={detailsData}
             setUserData={setDetailsData}
           />
@@ -79,7 +157,6 @@ const DetailsPersonal = () => {
             placeholder="Apellidos"
             name="detailsLastname"
             type="text"
-            value={detailsUser?.detailsLastname}
             userData={detailsData}
             setUserData={setDetailsData}
           />
@@ -87,7 +164,6 @@ const DetailsPersonal = () => {
             placeholder="Dirección"
             name="address"
             type="text"
-            value={detailsUser?.address}
             userData={detailsData}
             setUserData={setDetailsData}
           />
@@ -95,7 +171,6 @@ const DetailsPersonal = () => {
             placeholder="Código postal"
             name="postalCode"
             type="text"
-            value={detailsUser?.postalCode}
             userData={detailsData}
             setUserData={setDetailsData}
           />
@@ -103,7 +178,6 @@ const DetailsPersonal = () => {
             placeholder="Localidad"
             name="location"
             type="text"
-            value={detailsUser?.location}
             userData={detailsData}
             setUserData={setDetailsData}
           />
@@ -111,7 +185,6 @@ const DetailsPersonal = () => {
             placeholder="País"
             name="country"
             type="text"
-            value={detailsUser?.country}
             userData={detailsData}
             setUserData={setDetailsData}
           />
@@ -119,7 +192,6 @@ const DetailsPersonal = () => {
             placeholder="Teléfono"
             name="phone"
             type="text"
-            value={detailsUser?.phone}
             userData={detailsData}
             setUserData={setDetailsData}
           />
